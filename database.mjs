e76bb6cc226c0fddbfa49a8cdb95695036ad2899
nash115 <72687027+Nash115@users.noreply.github.com>
2025-02-06 21:04:23 +0100

    Première conversion avec node et express

---
+++ database.mjs
@@ -0,0 +1,158 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('./db/database.db', (err) => {
+    if (err) {
+        console.error('Database opening error: ', err);
+    } else {
+        console.log('Database opened');
+        db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, name TEXT, password TEXT, admin INTEGER, friends TEXT)');
+        db.run('CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, userid INTEGER, date TEXT, visibility INTEGER, title TEXT, content TEXT, mood INTEGER, weather INTEGER, FOREIGN KEY(userid) REFERENCES users(id))');
+    }
+});
+
+export function getUsers() {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM users', (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                rows.forEach(element => {
+                    element.friends = JSON.parse(element.friends);
+                });
+                resolve(rows);
+            }
+        });
+    })
+}
+
+export function insertUser(user) {
+    const query = `
+        INSERT INTO users (username, name, password, admin, friends)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+    const params = [
+        user.username,
+        user.name,
+        user.password,
+        user.admin,
+        JSON.stringify(user.friends)
+    ];
+    db.run(query, params, (err) => {
+        if (err) {
+            console.error('Insert user error: ', err);
+            return false;
+        } else {
+            console.log('User inserted successfully');
+            return true;
+        }
+    });
+}
+
+export function insertRecord(record) {
+    const query = `
+        INSERT INTO records (userid, date, visibility, title, content, mood, weather)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+    const params = [
+        record.userid,
+        record.date,
+        record.visibility,
+        record.title,
+        record.content,
+        record.mood,
+        record.weather
+    ];
+    db.run(query, params, function(err) {
+        if (err) {
+            console.error('Insert record error:', err);
+            return false;
+        } else {
+            console.log('Record inserted successfully');
+            return true;
+        }
+    });
+}
+
+export function chechAuth(username, password) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
+        db.get(query, [username, password], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (row) {
+                    row.friends = JSON.parse(row.friends);
+                }
+                resolve(row);
+            }
+        });
+    });
+}
+
+export function getUser(username) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM users WHERE username = ?`;
+        db.get(query, [username], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (row) {
+                    row.friends = JSON.parse(row.friends);
+                }
+                resolve(row);
+            }
+        });
+    });
+}
+
+export function deleteUser(userId) {
+    return new Promise((resolve, reject) => {
+        const query = `DELETE FROM users WHERE id = ?`;
+        db.run(query, [userId], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+export function getRecords(username, limit = undefined) {
+    const baseQuery = `SELECT * FROM records WHERE userid = (SELECT id FROM users WHERE username = ?) ORDER BY date DESC`;
+    if (limit === undefined) {
+        return new Promise((resolve, reject) => {
+            db.all(baseQuery, [username], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    } else {
+        return new Promise((resolve, reject) => {
+            const query = `${baseQuery} LIMIT ?`;
+            db.all(query, [username, limit], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+}
+
+export function deleteRecord(recordId) {
+    return new Promise((resolve, reject) => {
+        const query = `DELETE FROM records WHERE id = ?`;
+        db.run(query, [recordId], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
---
+++ server.mjs
@@ -0,0 +1,164 @@
+import express from 'express';
+import session from 'express-session';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import * as database from './database.mjs';
+
+const PORT = 80;
+const IP = '127.0.0.1'
+const app = express();
+// Gestion du chemin pour ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configuration d'EJS comme moteur de rendu
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: crypto.randomBytes(12).toString('hex'),
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+function tosha256(data) {
+    return crypto.createHash('sha256').update(data).digest('hex');
+}
+
+async function authenticateUser(username, passwordHash) {
+    let user = await database.chechAuth(username, passwordHash);
+    return user;
+}
+
+async function getUser(username) {
+    let user = await database.getUser(username);
+    return user;
+}
+
+async function registrationUser(user) {
+
+    let user_get = await getUser(user.username);
+
+    if (user_get) {
+        return false;
+    } else {
+        database.insertUser(user);
+        return true;
+    }
+}
+
+async function getTodayRecord(username) {
+    let record = await database.getRecords(username, 1);
+    return record[0];
+}
+
+async function getLastRecords(username) {
+    // TODO vérification si les jours sont dans la liste des 31 derniers jours
+    let records = await database.getRecords(username, 31);
+    records.shift();
+    return records;
+}
+
+async function getAllRecords(username) {
+    let records = await database.getRecords(username);
+    return records;
+}
+
+
+
+// Page d'accueil
+app.get('/', (req, res) => {
+    res.render('index', {user: req.session});
+});
+
+// Page de login
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+// Page de register
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+// Dashboard
+app.get('/dashboard', async (req, res) => {
+    if (req.session.username) {
+        res.render('dashboard', {
+            user: req.session,
+            today : await getTodayRecord(req.session.username),
+            jours : await getLastRecords(req.session.username)
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Page de login
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+
+
+
+// Login POST
+app.post('/login-post', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const hash = tosha256(password);
+
+    const auth = await authenticateUser(username, hash);
+
+    if (auth) {
+        req.session.username = auth.username;
+        req.session.admin = auth.admin;
+        req.session.name = auth.name;
+        req.session.friends = auth.friends;
+        res.redirect('/dashboard');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Register POST
+app.post('/register-post', async (req, res) => {
+    const username = req.body.username;
+    const name = req.body.name;
+    const password = req.body.password;
+    const password2 = req.body.passwordrepeat;
+    const hash = tosha256(password);
+
+    const reg = await registrationUser({
+        username: username,
+        name: name,
+        password: hash,
+        admin: 0,
+        friends: [],
+    });
+
+    if (reg) {
+        res.redirect('/login');
+    } else {
+        res.redirect('/register');
+    }
+});
+
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+});
+
+app.listen(PORT, IP, () => {
+    console.log(`Serveur lancé sur http://${IP}:${PORT}`);
+});
